690b3da3920e7dd0aff7d345fe7b3e4501e54088
Mirkium <alexandre.petitfrere@ynov.com>
2024-04-18 12:34:17 +0200

    commit_Header

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+//======================Header==========================\\
+
+var header = document.querySelector('header');
+
+header.innerHTML = `
+    <div class="header">
+        <a href="Accueil.html" class="img"><img src="../assets/img/LoL-Symbol.png"></a>
+        <a href="Ranked.html">RANKED</a>
+        <p>|</p>
+        <a href="Draft.html">DRAFT</a>
+        <p>|</p>
+        <a href="Patch.html">PATCH</a>
+        <p>|</p>
+        <a href="TempMode.html">TEMPORAL MODE</a>
+        <a href="Compte.html" class="compte"><img src="../assets/img/icon_perso.png"></a>
+    </div>
+`;
+
+//======================================================\\
+
