2e1f158c1e06f83d9ea4e2ab9396307a6e5a1c9c
Mirkium <alexandre.petitfrere@ynov.com>
2024-06-14 08:55:09 +0200

    for_git_pull

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,15 @@ var header = document.querySelector('header');
 
 header.innerHTML = `
     <div class="header">
-        <a href="Accueil.html" class="img"><img src="../assets/img/LoL-Symbol.png"></a>
-        <a href="Ranked.html">RANKED</a>
+        <a href="Accueil.html" class="img"><img src="/assets/img/LoL-Symbol.png"></a>
+        <a href="categories/ranked">RANKED</a>
         <p>|</p>
-        <a href="Draft.html">DRAFT</a>
+        <a href="categories/draft">DRAFT</a>
         <p>|</p>
-        <a href="Patch.html">PATCH</a>
+        <a href="categories/patch">PATCH</a>
         <p>|</p>
-        <a href="TempMode.html">TEMPORAL MODE</a>
-        <a href="Compte.html" class="compte"><img src="../assets/img/icon_perso.png"></a>
+        <a href="categories/temporal">TEMPORAL MODE</a>
+        <a href="Compte.html" class="compte"><img src="/assets/img/icon_perso.png"></a>
     </div>
 `;
 
@@ -26,7 +26,7 @@ var footer = document.querySelector('footer');
 
 footer.innerHTML = `
     <div class="footer">
-        <a href="https://www.leagueoflegends.com/fr-fr/"><img src="../assets/img/riot-games.png"></a>
+        <a href="https://www.leagueoflegends.com/fr-fr/"><img src="/assets/img/riot-games.png"></a>
         
         <div class="team">
             <p>Equipe :</p>
