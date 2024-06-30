cfd2941a915afe295c1696f60a24c2b04dba9cde
MentalOfCrow <145018989+MentalOfCrow@users.noreply.github.com>
2024-06-30 21:56:29 +0200

    update exportation controller pour profil + ejs footer et header update design + fichier ejs + css

--- site/controller/controller.js
+++ site/controller/controller.js
@@ -1,42 +1,42 @@
-
-
 exports.getAccueil = async (req, res) => {
-
     res.render('accueil');
 };
 
 exports.getLogin = async (req, res) => {
-
     res.render('login');
 };
 
 exports.getRegister = async (req, res) => {
-
     res.render('register');
 };
 
 exports.getProfil = async (req, res) => {
+    // sinon ca fonctionne pas pour le profil c'etait obvious as fuck 
+    const user = {
+        username: 'exemple_user',
+        email: 'exemple_user@example.com',
+        createdAt: new Date(),
+        bio: 'Ceci est une bio exemple.'
+    };
 
-    res.render('profil');
+    res.render('profil', { 
+        title: 'Profil',
+        user: user
+    });
 };
 
 exports.getGame = async (req, res) => {
-
     res.render('game');
 };
 
 exports.getUpdate = async (req, res) => {
-    
     res.render('update');
 };
 
-
 exports.getEsport = async (req, res) => {
-
     res.render('esport');
 };
 
 exports.getError = async (req, res) => {
-
     res.render('error');
 };
